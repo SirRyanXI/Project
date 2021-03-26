af199cc3804c4836df57f6f2aed4dc8dd8771028
Mao Karlos Sabornido <71705098+HonkaiBunny@users.noreply.github.com>
2021-03-26 14:52:01 +0000

    order page and function is working

---
+++ order/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'order'
+
+urlpatterns = [
+    path('history/', views.orderHistory, name='order_history'),
+    path('<int:order_id>/', views.viewOrder, name='order_detail'),
+]
