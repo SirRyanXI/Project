318d2c6cd445cde84e6957e5b75fdd5107eb91a0
Michael Roche <x00155240@mytudublin.ie>
2021-04-21 22:10:21 +0100

    added carousel, made manager and fixed search bar

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,7 +8,7 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age','email',)
+        fields = UserCreationForm.Meta.fields + ('email',)
 
 
 class CustomUserChangeForm(UserChangeForm):
