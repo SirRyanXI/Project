80760c43c712c6f12ee48f14432881c0aa2f978e
Ryan williamson <ryan.will617@gmail.com>
2021-04-25 16:04:21 +0100

    Add all products and cats

--- shop/admin.py
+++ shop/admin.py
@@ -7,7 +7,7 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category,CategoryAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name','price','description','category','available','stock','updated']
+    list_display = ['name','price','description','category','available','stock','created','updated']
     list_editable = ['price','stock','available']
     list_per_page = 20
 admin.site.register(Product,ProductAdmin)
